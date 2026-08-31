5f16e8a3f93645fcee49cfe9ae78a757cd642e14
Sitenova studio <skaikhsahil406@gmail.com>
2026-08-31 21:43:55 +0530

    Delete preload.js

--- preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  isDesktopApp: true,
-  getPrinters: () => ipcRenderer.invoke('get-printers'),
-  silentPrint: (payload) => ipcRenderer.invoke('silent-print', payload)
-});
